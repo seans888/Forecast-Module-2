fe52c1a442f06fa91aeacbf0148165747c84a917
jmadriano2 <jmadriano2@users.noreply.github.com>
2017-08-22 01:08:59 +0800

    Fixed Subsegment Forecast Problem 2

--- Prototype/PHPSCRIPTS/JASON/ForecastWithTimespan.php
+++ Prototype/PHPSCRIPTS/JASON/ForecastWithTimespan.php
@@ -28,53 +28,65 @@ if( isset($_POST['selectTimeSpan']) )
     $timeSpan = $_POST['selectTimeSpan'];
 }
 
-//insert dates on excel
-$date_query = "select date from(select * from room_actual where seg_id='rck' order by date desc limit $timeSpan) sub order by date asc;";
-$result = $conn->query($date_query);
-$x = 2;
-while($row = $result->fetch_assoc()){
-    $value = changeDashToComma($row["date"]);
-    $sheet -> setCellValueByColumnAndRow(0, $x, '=DATE('.$value.')');
-    $sheet -> setCellValueByColumnAndRow(5, 2, '=EOMONTH(DATE('.$value.'),1)');
-    $x++;
-}
+
+$segments = array('RCK','CORP','CORPO','PKG/PRM','WSOL','WSOF','INDO','INDR','CORPM','CON/ASSOC','GOV/NGO','GRPT','GRPO');
+
+$ctr = 0;
+
+while ($ctr != 13){
+    //set the designate sheets to go to
+    $sheet = $phpExcel ->setActiveSheetIndex($ctr);
+
+    //add defined names to new excel
+    $endofrange = $timeSpan+1;
+    $phpExcel->addNamedRange(new PHPExcel_NamedRange('timeline', $sheet, 'A2:A'.$endofrange));
+    $phpExcel->addNamedRange(new PHPExcel_NamedRange('rns', $sheet, 'B2:B'.$endofrange,true));
+    $phpExcel->addNamedRange(new PHPExcel_NamedRange('arr', $sheet, 'C2:C'.$endofrange,true));
+    $phpExcel->addNamedRange(new PHPExcel_NamedRange('rev', $sheet, 'D2:D'.$endofrange,true));
+
+    //insert dates on excel
+    $date_query = "select date from(select * from room_actual where seg_id='$segments[$ctr]' order by date desc limit $timeSpan) sub order by date asc;";
+    $result = $conn->query($date_query);
+    $x = 2;
+    while($row = $result->fetch_assoc()){
+        $value = changeDashToComma($row["date"]);
+        $sheet -> setCellValueByColumnAndRow(0, $x, '=DATE('.$value.')');
+        $sheet -> setCellValueByColumnAndRow(5, 2,'=EOMONTH(DATE('.$value.'),1)');
+        $x++;
+    }
 
 //insert room nights sold on excel
-$rns_query = "select ACTUAL_RNS from(select * from room_actual where seg_id='rck' order by date desc limit $timeSpan) sub order by date asc;";
-$result = $conn->query($rns_query);
-$x = 2;
-while($row = $result->fetch_assoc()){
-    $value = $row["ACTUAL_RNS"];
-    $sheet -> setCellValueByColumnAndRow(1, $x, $value);
-    $x++;
-}
+    $rns_query = "select ACTUAL_RNS from(select * from room_actual where seg_id='$segments[$ctr]' order by date desc limit $timeSpan) sub order by date asc;";
+    $result = $conn->query($rns_query);
+    $x = 2;
+    while($row = $result->fetch_assoc()){
+        $value = $row["ACTUAL_RNS"];
+        $sheet -> setCellValueByColumnAndRow(1, $x, $value);
+        $x++;
+    }
 
 //insert average room rate on excel
-$rns_query = "select ACTUAL_ARR from(select * from room_actual where seg_id='rck' order by date desc limit $timeSpan) sub order by date asc;";
-$result = $conn->query($rns_query);
-$x = 2;
-while($row = $result->fetch_assoc()){
-    $value = $row["ACTUAL_ARR"];
-    $sheet -> setCellValueByColumnAndRow(2, $x, $value);
-    $x++;
-}
+    $rns_query = "select ACTUAL_ARR from(select * from room_actual where seg_id='$segments[$ctr]' order by date desc limit $timeSpan) sub order by date asc;";
+    $result = $conn->query($rns_query);
+    $x = 2;
+    while($row = $result->fetch_assoc()){
+        $value = $row["ACTUAL_ARR"];
+        $sheet -> setCellValueByColumnAndRow(2, $x, $value);
+        $x++;
+    }
 
 //insert revenue on excel
-$rns_query = "select ACTUAL_REVENUE from(select * from room_actual where seg_id='rck' order by date desc limit $timeSpan) sub order by date asc;";
-$result = $conn->query($rns_query);
-$x = 2;
-while($row = $result->fetch_assoc()){
-    $value = $row["ACTUAL_REVENUE"];
-    $sheet -> setCellValueByColumnAndRow(3, $x, $value);
-    $x++;
+    $rns_query = "select ACTUAL_REVENUE from(select * from room_actual where seg_id='$segments[$ctr]' order by date desc limit $timeSpan) sub order by date asc;";
+    $result = $conn->query($rns_query);
+    $x = 2;
+    while($row = $result->fetch_assoc()){
+        $value = $row["ACTUAL_REVENUE"];
+        $sheet -> setCellValueByColumnAndRow(3, $x, $value);
+        $x++;
+    }
+    $ctr++;
 }
 
-//add defined names to new excel
-$endofrange = $timeSpan+1;
-$phpExcel->addNamedRange(new PHPExcel_NamedRange('timeline', $sheet, 'A2:A'.$endofrange));
-$phpExcel->addNamedRange(new PHPExcel_NamedRange('rns', $sheet, 'B2:B'.$endofrange));
-$phpExcel->addNamedRange(new PHPExcel_NamedRange('arr', $sheet, 'C2:C'.$endofrange));
-$phpExcel->addNamedRange(new PHPExcel_NamedRange('rev', $sheet, 'D2:D'.$endofrange));
 
 
 //functions
